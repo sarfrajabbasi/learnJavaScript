c129f3edec73ed75c4b13ddae2420eb157199d59
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-01 21:14:53 +0530

    prtactice

---
+++ practice-1/JS/practice.js
@@ -0,0 +1,35 @@
+const arr = ["sarfraj","asif","asif","sufiyan","sufiyan","toqeere","sarfraj","junaid","junaid","sarfraj","sarfraj","sarfraj"];
+
+const arr2 =[];
+
+for(let item of arr){
+    if(!arr2.includes(item)){
+        arr2.push(item)
+    }
+}
+console.log(arr2);
+
+const roadmap = {
+    name:'JavaScript',
+};
+
+Object.freeze(roadmap);
+
+roadmap.name = "JavaScript Roadmap";
+
+console.log(roadmap.name);
+
+let abc = arr.map((a)=>{
+    a = a.charAt(0).toUpperCase() + a.slice(1);
+    return a
+ })
+
+ console.log(abc);
+
+ for(let i=0;i<5;i++){
+    if(i===1){
+        continue;
+    }
+    console.log(i);
+ }
+
